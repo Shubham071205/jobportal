e70a71297942b9f5450a07c33744f9ab8164d94e
Shubham071205 <143300384+Shubham071205@users.noreply.github.com>
2025-12-11 12:07:51 +0530

    Add files via upload

---
+++ jobportal/jobportal/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path
+from myapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('home/', views.home, name='home_page'),
+
+    path('login-jobseeker/', views.login_jobseeker, name='login_jobseeker'),
+    path('signup-jobseeker/', views.signup_jobseeker, name='signup_jobseeker'),
+    path('jobseeker/dashboard/', views.jobseeker_dashboard, name='jobseeker_dashboard'),
+
+    path('login-jobprovider/', views.login_jobprovider, name='login_jobprovider'),
+    path('signup-jobprovider/', views.signup_jobprovider, name='signup_jobprovider'),
+
+
+    path('logout/', views.logout_view, name='logout'),
+]
